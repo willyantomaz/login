4578f86eed11db06e1f8443ed08a1015d8b18ffe
willyantomaz <willyantomaz@usacucar.com.br>
2025-01-17 10:29:42 -0300

    first commit

---
+++ src/test/java/br/com/pratica/login/LoginApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.pratica.login;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LoginApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
